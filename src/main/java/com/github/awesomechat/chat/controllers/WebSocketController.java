d8dc94e4dc6d33afe9f94dec0b68f790dacc5880
root <secret>
2017-08-03 10:04:56 +0400

    rework

--- src/main/java/com/github/awesomechat/chat/controllers/WebSocketController.java
+++ src/main/java/com/github/awesomechat/chat/controllers/WebSocketController.java
@@ -1,13 +1,22 @@
 package com.github.awesomechat.chat.controllers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.awesomechat.chat.messages.Message;
 import com.github.awesomechat.chat.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @Controller
@@ -16,6 +25,8 @@ public class WebSocketController extends TextWebSocketHandler {
     @Autowired
     private UserService userService;
 
+    private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
 /*    private SimpMessagingTemplate template;
 
     @Autowired
@@ -23,6 +34,44 @@ public class WebSocketController extends TextWebSocketHandler {
         this.template = template;
     }*/
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        /*Создать бин джсона
+        * Заинжектить
+        * Входящее раскодировать из джсона
+        * Узнать его тип
+        * блок свич по каждому типу создает ответное сообщение
+        * броадкаст обратно
+        * */
+/*        Message m = new Message();
+        TextMessage mmm = new TextMessage("");
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        //set text to message
+        //send m*/
+
+        for(WebSocketSession wss : sessions) {
+            try {
+                wss.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        sessions.remove(session);
+    }
 
     @RequestMapping(value = "/chat")
     public void toClient() {
--- src/main/java/com/github/awesomechat/chat/messages/JoinMessage.java
+++ src/main/java/com/github/awesomechat/chat/messages/JoinMessage.java
@@ -2,6 +2,13 @@ package com.github.awesomechat.chat.messages;
 
 
 public class JoinMessage extends Message {
+    public static final int JOIN = 1;
+    public static final int MESSAGE = 2;
+    public static final int GET_USERS = 3;
+    public static final int USER_LIST = 4;
+    public static final int GET_MESSAGES = 5;
+    public static final int MESSAGE_LIST = 6;
+
     private String name;
 
     public String getName() {
