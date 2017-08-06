31b22ae22070624cc9f001ff344516996be8bbaa
PanicLab <PanicLabLimited@gmail.com>
2017-08-06 15:10:15 +0400

    before add new registration controller

--- src/main/java/com/github/awesomechat/chat/controllers/RegisterController.java
+++ src/main/java/com/github/awesomechat/chat/controllers/RegisterController.java
@@ -1,20 +1,13 @@
 package com.github.awesomechat.chat.controllers;
 
-import com.github.awesomechat.chat.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
 
-    @Autowired
-    private UserService userService;
-
     @RequestMapping("/register")
     public String register() {
         return "register";
     }
-
-
 }
